e6188e7cdbaf3d65a9fd1074cf5c26248285c93f
takahara tomoya <t.tkorock221@gmail.com>
2022-09-12 22:58:33 +0900

    suspense

--- src/components/fetch.tsx
+++ src/components/fetch.tsx
@@ -5,7 +5,8 @@ import { css } from '@emotion/css';
 // React18からはFCからはchildrenの型定義が削除されたので定義する必要あり
 export const Fetch: React.FC = () =>{
 
-    const { data, error } = useSWR('/api/weather', fetcher)
+    const { data, error } = useSWR('/api/weather', fetcher ,{ suspense: true })
+    console.log(data,error)
     const lists = data ? data.forecasts.map((d) => <div key={d.date}>{d.telop}</div>): "";
     return (
         <div>{lists}</div>
@@ -22,5 +23,11 @@ type API = {
     forecasts: Obj[]
 }
 
-const fetcher = ():Promise<API> => fetch('/api/weather').then(res => res.json())
+const sleep = (sec:number) => new Promise(resolve => setTimeout(resolve, sec * 1000));
+
+const fetcher = async ():Promise<API> => {
+    await sleep(5);
+    return fetch('/api/weather').then(res => res.json())
+};
+
 
